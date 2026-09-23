c92be5dbf15193b7b836c224ab67175ce818afd0
lucasfgaldinos <lucasfgaldinos@outlook.com>
2026-09-22 22:27:20 -0300

    config: adiciona cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
+import cors from 'cors';
 import express from 'express';
 import { routes } from './routes.js';
 
 export const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
